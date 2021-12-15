eaba8c82c735242eea77f8f7e93421a12b5de23b
Simone <startae14@gmail.com>
2021-12-15 15:32:07 +0100

    Create a CardListItem coomponent

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@
 import { Row, Col, Card } from 'react-bootstrap'
 import PageLayout from 'components/PageLayout'
 import AuthorIntro from 'components/AuthorIntro'
+import CardListItem from 'components/CardListItem'
+
 export default function Home() {
   return (
     <PageLayout>
@@ -10,32 +12,7 @@ export default function Home() {
       {/* className from props */}
       <Row className="mb-5">
         <Col md="10">
-          {/* CardListItem STARTS */}
-          <Card className={`fj-card fj-card-list`}>
-            <div className="card-body-wrapper">
-              <Card.Header
-                className="d-flex flex-row">
-                <img
-                  src={'https://via.placeholder.com/150'}
-                  className="rounded-circle mr-3"
-                  height="50px"
-                  width="50px"
-                  alt="avatar"/>
-                  <div>
-                    <Card.Title className="font-weight-bold mb-1">Placeholder Author</Card.Title>
-                    <Card.Text className="card-date">Placeholder Date</Card.Text>
-                  </div>
-              </Card.Header>
-              <Card.Body>
-                <Card.Title className="card-main-title">Placeholder Title</Card.Title>
-                <Card.Text>Placehodler Subtitle</Card.Text>
-              </Card.Body>
-            </div>
-            <a href="#" className="card-button">
-              Read More
-            </a>
-          </Card>
-          {/* CardListItem ENDS */}
+          <CardListItem />
         </Col>
 
         <Col md="4">
