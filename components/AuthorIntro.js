676b03094519aa1b2150787a031326476ebe86d0
Simone <startae14@gmail.com>
2021-12-15 15:28:04 +0100

    Create an AuthorIntro coomponent

---
+++ components/AuthorIntro.js
@@ -0,0 +1,29 @@
+import { Row, Col, Media, Image } from 'react-bootstrap'
+
+const AuthorIntro = () => {
+  return (
+    <Row>
+      <Col md="8">
+        <Media className="mb-4 admin-intro">
+          <Image
+            roundedCircle
+            width={64}
+            height={64}
+            className="mr-3"
+            src="https://avatars.githubusercontent.com/u/2589748?v=4"
+            alt="Generic placeholder"
+          />
+          <Media.Body>
+            <h5 className="font-weight-bold mb-0">Hello Friends,</h5>
+            <p className="welcome-text">
+            My name is Simone Taeggi and I am an experienced software engineer.
+            and this is my blog page.
+            </p>
+          </Media.Body>
+        </Media>
+      </Col>
+    </Row>
+  );
+};
+
+export default AuthorIntro;
