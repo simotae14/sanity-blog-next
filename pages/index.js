323f788e33750e1dc20aeaae9e4ecbfc358044a8
Simone <startae14@gmail.com>
2021-12-15 15:35:48 +0100

    Create a CardItem componnent

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-/* eslint-disable @next/next/no-img-element */
-import { Row, Col, Card } from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap'
 import PageLayout from 'components/PageLayout'
 import AuthorIntro from 'components/AuthorIntro'
 import CardListItem from 'components/CardListItem'
+import CardItem from 'components/CardItem'
 
 export default function Home() {
   return (
@@ -16,36 +16,7 @@ export default function Home() {
         </Col>
 
         <Col md="4">
-          <Card className={`fj-card`}>
-            <div className="card-body-wrapper">
-              <Card.Header
-                className="d-flex flex-row">
-                <img
-                  src={'https://via.placeholder.com/150'}
-                  className="rounded-circle mr-3"
-                  height="50px"
-                  width="50px"
-                  alt="avatar"/>
-                <div>
-                  <Card.Title className="font-weight-bold mb-1">Placeholder Author</Card.Title>
-                  <Card.Text className="card-date">Placeholder Date</Card.Text>
-                </div>
-              </Card.Header>
-              <div className="view overlay">
-                <Card.Img
-                  src='https://via.placeholder.com/250'
-                  alt="Card image cap"
-                />
-              </div>
-              <Card.Body>
-                <Card.Title className="card-main-title">Placeholder Title</Card.Title>
-                <Card.Text>Placehodler Subtitle</Card.Text>
-              </Card.Body>
-            </div>
-            <a className="card-button">
-              Read More
-            </a>
-          </Card>
+          <CardItem />
         </Col>
       </Row>
     </PageLayout>
