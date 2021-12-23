514255b1acd0b5fc9e4ccdecaae0e69af0cd522e
Simone <startae14@gmail.com>
2021-12-23 15:17:10 +0100

    Add Google Font to a Next App

--- components/PageLayout.js
+++ components/PageLayout.js
@@ -1,21 +1,30 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import { Container } from 'react-bootstrap'
+import Head from 'next/head'
 import Navbar from 'components/Navbar'
 
 const PageLayout = ({ children, className }) => {
   return (
-    <Container>
-      <Navbar />
-      <div className={`page-wrapper ${className}`}>
-        {children}
-      </div>
-      <footer className="page-footer">
-        <div>
-          <a href="#">courses</a>{' | '}
-          <a href="#">github</a>{' | '}
-          <a href="#">facebook</a>
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;900&display=swap" rel="stylesheet" />
+      </Head>
+      <Container>
+        <Navbar />
+        <div className={`page-wrapper ${className}`}>
+          {children}
         </div>
-      </footer>
-    </Container>
+        <footer className="page-footer">
+          <div>
+            <a href="#">courses</a>{' | '}
+            <a href="#">github</a>{' | '}
+            <a href="#">facebook</a>
+          </div>
+        </footer>
+      </Container>
+    </>
   )
 }
 
