8d746129923428a94fcd0441f017c1e6259a21fa
Simone <startae14@gmail.com>
2021-12-23 18:03:51 +0100

    Use getStaticPaths inside the [slug].js to generate the data for every single blog page

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -11,12 +11,35 @@ const BlogDetail = ({ blog }) => {
   );
 }
 
-export async function getServerSideProps({ params }) {
+export async function getStaticProps({ params }) {
   const blog = await getBlogBySlug(params.slug);
   return {
     props: {
       blog
     }
+  }
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: {
+          slug: 'my-third-blog'
+        }
+      },
+      {
+        params: {
+          slug: 'my-second-blog'
+        }
+      },
+      {
+        params: {
+          slug: 'my-first-blog'
+        }
+      }
+    ],
+    fallback: false
   }
 }
 
