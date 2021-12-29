0308ea688652187fc270fa9b491937bfa4c8caef
Simone <startae14@gmail.com>
2021-12-29 19:41:16 +0100

    Improve Images, definining a serializer

--- components/BlogContent.js
+++ components/BlogContent.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import BlockContent from '@sanity/block-content-to-react'
 import HighlightCode from 'components/HighlightCode'
 
@@ -18,6 +19,17 @@ const serializers = {
           {code}
         </HighlightCode>
       )
+    },
+    image: ({ node: {
+      asset,
+      alt
+    } }) => {
+      return (
+        <div className="blog-image">
+          <img alt={alt} src={asset.url} />
+          <div className="image-alt">{alt}</div>
+        </div>
+      )
     }
   }
 }
@@ -27,11 +39,6 @@ const BlogContent = ({ content }) => {
     <BlockContent
       blocks={content}
       serializers={serializers}
-      imageOptions={{
-        w: 320,
-        h: 240,
-        fit: 'max'
-      }}
     />
   );
 };
