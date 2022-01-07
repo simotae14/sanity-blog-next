5b3bad0186649efbf56fd23dd58ca6a2a3fecbf8
Simone <startae14@gmail.com>
2022-01-07 12:07:36 +0100

    Add Fontawesome library, use the icons to toggle the Results view

--- components/FilteringMenu.js
+++ components/FilteringMenu.js
@@ -1,3 +1,7 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const LIST_VIEW_ICONS = ['list', 'border-all'];
+
 const FilteringMenu = ({ filter, onChange }) => {
   return (
     <div className="filtering-menu mb-2">
@@ -6,7 +10,16 @@ const FilteringMenu = ({ filter, onChange }) => {
           list: +!filter.view.list
         }); // the first parameter is the key inside the state filter
       }}>
-        List Filter - { filter.view.list }
+        <FontAwesomeIcon
+          className="clickable hoverable"
+          size="2x"
+          icon={LIST_VIEW_ICONS[filter.view.list]}
+          onClick={() => {
+            onChange('view', {
+              list: +!filter.view.list
+            });
+          }}
+        />
       </div>
     </div>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,17 @@
+// import Fontawesome library and config
+import { library, config } from '@fortawesome/fontawesome-svg-core'
+// import the 2 fontawesome icons
+import {
+  faBorderAll,
+  faList
+} from '@fortawesome/free-solid-svg-icons'
+// disable automatic import of fontawesome css
+config.autoAddCss = false;
+// add the icons to the library
+library.add(faList, faBorderAll);
+
+// import Fontawesome style
+import '@fortawesome/fontawesome-svg-core/styles.css'
 // import bootstrap css
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'highlight.js/styles/base16/darcula.css'
