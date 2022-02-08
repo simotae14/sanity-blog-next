6085604be4a3bc1c426a5cf157d46167ce191b60
Simone <startae14@gmail.com>
2022-02-09 00:16:53 +0100

    Fetch more blogs inside the getBlogs function

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,6 +1,8 @@
 import { getAllBlogs } from 'lib/api'
 
 export default async function getBlogs(req, res) {
-  const data = await getAllBlogs();
+  // retrieve offset and transform it in a number
+  const offset = parseInt((req.query.offset || 0), 10)
+  const data = await getAllBlogs({offset});
   res.status(200).json(data)
 }
